e46bef79017ef28d2989bf126d0f912cf662b500
jdg2-newman <jacknewman1994@hotmail.co.uk>
2022-06-06 20:53:53 +0100

    updated controller

---
+++ CityInfo3.API/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo3.API.Controllers
+{
+	[ApiController]
+	public class CitiesController : ControllerBase
+	{
+		[HttpGet("api/cities")]
+		public JsonResult GetCities()
+		{
+			return new JsonResult(
+				new List<object>
+				{
+					new { id = 1, Name = "New York City" },
+					new { id = 2, Name = "Ram Ranch City" }
+				});
+
+		}
+	}
+}
+
