8072f90d14f348b488e579b607702a602f2efa16
jdg2-newman <jacknewman1994@hotmail.co.uk>
2022-07-11 11:52:37 -0700

    added db stuff

---
+++ CityInfo3.API/DbContexts/CityInfoContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CityInfo3.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CityInfo3.API.DbContexts
+{
+    public class CityInfoContext: DbContext
+    {
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options ):base(options)
+        {
+
+        }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder  optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("connectionString");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+        
+    }
+}
+
---
+++ CityInfo3.API/Entities/City.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CityInfo3.API.Models;
+
+namespace CityInfo3.API.Entities
+{
+    public class City
+	{
+        [Key]
+		[DatabaseGenerated (DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string Name { get; set; }
+
+		[MaxLength(200)]
+		public string? Description { get; set; }
+
+
+		public ICollection<PointOfInterest> PointsOfInterest { get; set; }
+		= new List<PointOfInterest>();
+
+
+		public City(string name)
+        {
+			Name = name;
+        }
+
+	
+}
+}
+
